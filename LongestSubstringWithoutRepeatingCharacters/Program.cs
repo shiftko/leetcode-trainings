ba34817ad553f5e7868554ce2f79bbbba4c19c6b
idoma <h2870132@gmail.com>
2023-03-09 07:44:36 +0200

    Runtime: 101 ms (Beats: 41.74%) | Memory: 39.5 MB (Beats: 77.77%)

--- LongestSubstringWithoutRepeatingCharacters/Program.cs
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+int LengthOfLongestSubstring(string s)
+{
+    var maxLen = 0;
+    var chars = new Dictionary<char, int>();
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (chars.ContainsKey(s[i]))
+        {
+            i = chars[s[i]];
+            maxLen = Math.Max(maxLen, chars.Count);
+            chars.Clear();
+        }
+        else
+        {
+            chars.Add(s[i], i);
+        }
+    }
+
+    return Math.Max(maxLen, chars.Count);
+}
+
+// expected 3
+Console.WriteLine(LengthOfLongestSubstring("pwwkew"));
+
+// expected 3
+Console.WriteLine(LengthOfLongestSubstring("abcabcbb"));
