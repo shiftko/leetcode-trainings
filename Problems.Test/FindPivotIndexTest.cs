96fedc7552c10e79517f70c43cc282ab49a5a5ee
idoma <h2870132@gmail.com>
2023-03-09 23:55:26 +0200

    FindPivotIndex

---
+++ Problems.Test/FindPivotIndexTest.cs
@@ -0,0 +1,33 @@
+﻿namespace Problems.Test;
+
+using FPI = FindPivotIndex;
+
+public class FindPivotIndexTest
+{
+    private readonly Dictionary<int[], int> _testData = new()
+    {
+        { new[] { 1, 7, 3, 6, 5, 6 }, 3 },
+        { new[] { 1, 2, 3 }, -1 },
+        { new[] { 2, 1, -1 }, 0 },
+        { new[] { -1, -1, 0, 1, 1, 0 }, 5 },
+        { new[] { -1, -1, 1, 1, 0, 0 }, 4 },
+    };
+
+    [Fact]
+    public void PivotIndexTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            Assert.Equal(kvp.Value, FPI.PivotIndex(kvp.Key));
+        }
+    }
+
+    [Fact]
+    public void PivotIndex2Test()
+    {
+        foreach (var kvp in _testData)
+        {
+            Assert.Equal(kvp.Value, FPI.PivotIndex2(kvp.Key));
+        }
+    }
+}
---
+++ Problems/FindPivotIndex.cs
@@ -0,0 +1,68 @@
+﻿namespace Problems;
+
+public class FindPivotIndex
+{
+    public static int PivotIndex(int[] nums)
+    {
+        var index = -1;
+        var baseSum = nums.Sum();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if ((i == 0 || i == nums.Length - 1) && baseSum - nums[i] == 0)
+            {
+                return i;
+            }
+
+            var li = i;
+            var leftSum = 0;
+            while (li > 0)
+            {
+                leftSum += nums[--li];
+            }
+
+            var ri = i;
+            var rightSum = 0;
+            while (ri < nums.Length - 1)
+            {
+                rightSum += nums[++ri];
+            }
+
+            if (leftSum == rightSum)
+            {
+                return i;
+            }
+        }
+
+        return index;
+    }
+
+    public static int PivotIndex2(int[] nums)
+    {
+        int index = -1;
+        int lastIndex = nums.Length - 1;
+        int baseSum = nums.Sum();
+
+        if (baseSum - nums[0] == 0)
+        {
+            return 0;
+        }
+
+        var leftSum = nums[0];
+        for (int i = 1; i < lastIndex; i++)
+        {
+            if (leftSum == baseSum - leftSum - nums[i])
+            {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        if (baseSum - nums[lastIndex] == 0)
+        {
+            return lastIndex;
+        }
+
+        return index;
+    }
+}
