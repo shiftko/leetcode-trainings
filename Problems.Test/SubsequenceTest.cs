36d4f8df9e6f5b445b2380cad91766a5ee99b6f7
idoma <h2870132@gmail.com>
2023-03-10 23:46:10 +0200

    IsSubsequence

---
+++ Problems.Test/SubsequenceTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Problems.Test;
+
+public class SubsequenceTest
+{
+    private readonly Dictionary<Dictionary<char, string>, bool> _testData = new()
+    {
+        { new Dictionary<char, string> { { 's', "abc" }, { 't', "ahbgdc" } }, true },
+        { new Dictionary<char, string> { { 's', "axc" }, { 't', "ahbgdc" } }, false },
+        { new Dictionary<char, string> { { 's', "aaaaaa" }, { 't', "bbaaaa" } }, false },
+        { new Dictionary<char, string> { { 's', "ab" }, { 't', "baab" } }, true },
+    };
+
+    [Fact]
+    public void IsSubsequenceTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            Assert.Equal(kvp.Value, Subsequence.IsSubsequence(kvp.Key['s'], kvp.Key['t']));
+        }
+    }
+}
---
+++ Problems/Subsequence.cs
@@ -0,0 +1,20 @@
+﻿namespace Problems;
+
+public static class Subsequence
+{
+    public static bool IsSubsequence(string s, string t)
+    {
+        foreach (var chr in s)
+        {
+            var it = t.IndexOf(chr);
+            if (it == -1)
+            {
+                return false;
+            }
+
+            t = t.Substring(it + 1);
+        }
+
+        return true;
+    }
+}
