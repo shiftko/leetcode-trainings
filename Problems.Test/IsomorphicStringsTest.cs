9d137bdc6f815e10391a9ba82b28f774499888e0
idoma <h2870132@gmail.com>
2023-03-10 16:35:46 +0200

    IsomorphicStrings

---
+++ Problems.Test/IsomorphicStringsTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems.Test;
+
+using IS = IsomorphicStrings;
+
+public class IsomorphicStringsTest
+{
+    private readonly Dictionary<Dictionary<char, string>, bool> _testData = new()
+    {
+        { new Dictionary<char, string> { { 's', "egg" }, { 't', "add" } }, true },
+        { new Dictionary<char, string> { { 's', "foo" }, { 't', "bar" } }, false },
+        { new Dictionary<char, string> { { 's', "paper" }, { 't', "title" } }, true },
+        { new Dictionary<char, string> { { 's', "xsxs" }, { 't', "baba" } }, true },
+        { new Dictionary<char, string> { { 's', "badc" }, { 't', "baba" } }, false },
+    };
+
+    [Fact]
+    public void IsIsomorphicTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            Assert.Equal(kvp.Value, IS.IsIsomorphic(kvp.Key['s'], kvp.Key['t']));
+        }
+    }
+}
---
+++ Problems/IsomorphicStrings.cs
@@ -0,0 +1,35 @@
+﻿namespace Problems;
+
+public static class IsomorphicStrings
+{
+    public static bool IsIsomorphic(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        var relations = new Dictionary<char, char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (relations.ContainsKey(s[i]))
+            {
+                if (relations[s[i]] != t[i])
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (relations.ContainsValue(t[i]))
+                {
+                    return false;
+                }
+
+                relations.Add(s[i], t[i]);
+            }
+        }
+
+        return true;
+    }
+}
