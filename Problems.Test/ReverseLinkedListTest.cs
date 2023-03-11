3f7b1d375e98ee41159b19a5d43fca199650fb11
idoma <h2870132@gmail.com>
2023-03-11 18:41:57 +0200

    ReverseLinkedList

---
+++ Problems.Test/ReverseLinkedListTest.cs
@@ -0,0 +1,25 @@
+﻿using Problems.Shared;
+using _llh = Problems.Shared.LinkedListHelper;
+
+namespace Problems.Test;
+
+public class ReverseLinkedListTest
+{
+    private readonly Dictionary<int[], int[]> _testData = new()
+    {
+        { new[] { 1, 1, 2, 3, 4, 4 }, new[] { 4, 4, 3, 2, 1, 1 } },
+    };
+
+    [Fact]
+    public void ReverseListTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            var headNode = _llh.CreateSequenceAndGetHead(kvp.Key);
+
+            ListNode? headNodeRes = ReverseLinkedList.ReverseList(headNode);
+
+            Assert.Equal(kvp.Value, _llh.GetValues(headNodeRes));
+        }
+    }
+}
---
+++ Problems/ReverseLinkedList.cs
@@ -0,0 +1,21 @@
+﻿using Problems.Shared;
+
+namespace Problems;
+
+public static class ReverseLinkedList
+{
+    public static ListNode? ReverseList(ListNode? node)
+    {
+        ListNode? prev = default;
+        ListNode? current = node;
+        while (current is not null)
+        {
+            var setupNext = current.Next;
+            current.Next = prev;
+            prev = current;
+            current = setupNext;
+        }
+
+        return prev;
+    }
+}
