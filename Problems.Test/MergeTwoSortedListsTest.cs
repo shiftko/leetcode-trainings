6fcd44695ff4383df356a926e964126991a8b3f4
idoma <h2870132@gmail.com>
2023-03-11 13:52:52 +0200

    MergeTwoSortedLists

---
+++ Problems.Test/MergeTwoSortedListsTest.cs
@@ -0,0 +1,50 @@
+﻿namespace Problems.Test;
+
+public class MergeTwoSortedListsTest
+{
+    private readonly Dictionary<Dictionary<string, int[]>, int[]> _testData = new()
+    {
+        { new() { { "set1", new[] { 1, 2, 4 } }, { "set2", new[] { 1, 3, 4 } } }, new[] { 1, 1, 2, 3, 4, 4 } },
+        { new() { { "set1", new[] { 1, 2, 4 } }, { "set2", new int[] { } } }, new[] { 1, 2, 4 } },
+        { new() { { "set1", new int[] { } }, { "set2", new[] { 4, 5, 6 } } }, new[] { 4, 5, 6 } },
+        { new() { { "set1", new int[] { } }, { "set2", new int[] { } } }, new int[] { } },
+    };
+
+    [Fact]
+    public void MergeTwoListsTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            var headNode1 = CreateSequenceAndGetHead(kvp.Key["set1"]);
+            var headNode2 = CreateSequenceAndGetHead(kvp.Key["set2"]);
+
+            var headNodeRes = MergeTwoSortedLists.MergeTwoLists(headNode1, headNode2);
+
+            Assert.Equal(kvp.Value, GetValues(headNodeRes));
+        }
+    }
+
+    private static ListNode? CreateSequenceAndGetHead(IReadOnlyList<int> set)
+    {
+        ListNode? headNode = null;
+        for (var i = set.Count - 1; i >= 0; i--)
+        {
+            var currentNode = new ListNode(set[i], headNode);
+            headNode = currentNode;
+        }
+
+        return headNode;
+    }
+
+    private static int[] GetValues(ListNode? node)
+    {
+        var values = new List<int>();
+        while (node is not null)
+        {
+            values.Add(node.Val);
+            node = node.Next;
+        }
+
+        return values.ToArray();
+    }
+}
---
+++ Problems/MergeTwoSortedLists.cs
@@ -0,0 +1,84 @@
+﻿namespace Problems;
+
+public static class MergeTwoSortedLists
+{
+    public static ListNode? MergeTwoLists(ListNode? list1, ListNode? list2)
+    {
+        ListNode? headNode;
+        ListNode? currentNode;
+
+        if (list1 is null && list2 is null)
+        {
+            return null;
+        }
+
+        if (list1 is null)
+        {
+            return list2;
+        }
+
+        if (list2 is null)
+        {
+            return list1;
+        }
+
+        if (list1.Val > list2.Val)
+        {
+            headNode = list2;
+            currentNode = headNode;
+            list2 = list2.Next;
+        }
+        else
+        {
+            headNode = list1;
+            currentNode = headNode;
+            list1 = list1.Next;
+        }
+
+        while (list1 != null || list2 != null)
+        {
+            if (list1 is null)
+            {
+                currentNode.Next = list2;
+                currentNode = list2;
+                list2 = list2.Next;
+                continue;
+            }
+
+            if (list2 is null)
+            {
+                currentNode.Next = list1;
+                currentNode = list1;
+                list1 = list1.Next;
+                continue;
+            }
+
+            if (list1.Val > list2.Val)
+            {
+                currentNode.Next = list2;
+                currentNode = list2;
+                list2 = list2.Next;
+            }
+            else
+            {
+                currentNode.Next = list1;
+                currentNode = list1;
+                list1 = list1.Next;
+            }
+        }
+
+        return headNode;
+    }
+}
+
+public class ListNode
+{
+    public int Val;
+    public ListNode? Next;
+
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.Val = val;
+        this.Next = next;
+    }
+}
