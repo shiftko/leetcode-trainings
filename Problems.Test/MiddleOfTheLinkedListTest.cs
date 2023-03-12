d7d1853f9871d95bbbdae7886dfa698ab5fd56af
idoma <h2870132@gmail.com>
2023-03-12 15:07:24 +0200

    GetMiddleNode

---
+++ Problems.Test/MiddleOfTheLinkedListTest.cs
@@ -0,0 +1,25 @@
+﻿using _llh = Problems.Shared.LinkedListHelper;
+
+namespace Problems.Test;
+
+public class MiddleOfTheLinkedListTest
+{
+    private readonly Dictionary<int[], int[]> _testData = new()
+    {
+        { new[] { 1, 2, 3, 4, 5 }, new[] { 3, 4, 5 } },
+        { new[] { 1, 2, 3, 4, 5, 6 }, new[] { 4, 5, 6 } },
+    };
+
+    [Fact]
+    public void GetMiddleNodeTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            var node = _llh.CreateSequenceAndGetHead(kvp.Key);
+
+            var midNode = MiddleOfTheLinkedList.GetMiddleNode(node);
+
+            Assert.Equal(kvp.Value, _llh.GetValues(midNode));
+        }
+    }
+}
---
+++ Problems/MiddleOfTheLinkedList.cs
@@ -0,0 +1,25 @@
+﻿using Problems.Shared;
+
+namespace Problems;
+
+public class MiddleOfTheLinkedList
+{
+    public static ListNode? GetMiddleNode(ListNode? head)
+    {
+        var counter = 0;
+        var midNode = head;
+
+        while (head is not null)
+        {
+            counter++;
+            if (counter % 2 == 0)
+            {
+                midNode = midNode?.Next;
+            }
+
+            head = head.Next;
+        }
+
+        return midNode;
+    }
+}
