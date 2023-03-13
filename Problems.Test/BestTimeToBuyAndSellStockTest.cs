ed7c39d9c1d4c34676ae1dc2fd69f499ca96c4d2
idoma <h2870132@gmail.com>
2023-03-13 12:26:55 +0200

    BestTimeToBuyAndSellStock

---
+++ Problems.Test/BestTimeToBuyAndSellStockTest.cs
@@ -0,0 +1,23 @@
+﻿using BT = Problems.BestTimeToBuyAndSellStock;
+
+namespace Problems.Test;
+
+public class BestTimeToBuyAndSellStockTest
+{
+    private readonly Dictionary<int[], int> _testData = new()
+    {
+        { new[] { 7, 1, 5, 3, 6, 4 }, 5 },
+        { new[] { 7, 6, 4, 3, 1 }, 0 },
+        { new[] { 1, 2 }, 1 },
+    };
+
+    [Fact]
+    public void GetMaxProfitTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            var res = BT.GetMaxProfit(kvp.Key);
+            Assert.Equal(kvp.Value, res);
+        }
+    }
+}
---
+++ Problems/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,23 @@
+﻿namespace Problems;
+
+public static class BestTimeToBuyAndSellStock
+{
+    // bad implementation
+    public static int GetMaxProfit(int[] prices)
+    {
+        var maxProfit = 0;
+        for (int i = 0; i < prices.Length - 1; i++)
+        {
+            for (int j = i + 1; j < prices.Length; j++)
+            {
+                var profit = prices[j] - prices[i];
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
