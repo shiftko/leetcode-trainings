654efd9ddd13c07e750039ad63f5d2bb0f3038a6
idoma <h2870132@gmail.com>
2023-03-13 18:24:13 +0200

    LongestPalindrome

---
+++ Problems.Test/LongestPalindromeTest.cs
@@ -0,0 +1,21 @@
+﻿namespace Problems.Test;
+
+public class LongestPalindromeTest
+{
+    private readonly Dictionary<string, int> _testData = new()
+    {
+        { "abccccdd", 7 },
+        { "bb", 2 },
+        { "a", 1 },
+    };
+
+
+    [Fact]
+    public void GetLongestPalindromeTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            Assert.Equal(kvp.Value, LongestPalindrome.GetLongestPalindrome(kvp.Key));
+        }
+    }
+}
---
+++ Problems/LongestPalindrome.cs
@@ -0,0 +1,37 @@
+﻿namespace Problems;
+
+public static class LongestPalindrome
+{
+    public static int GetLongestPalindrome(string s)
+    {
+        var polLen = 0;
+        var chars = new Dictionary<char, int>();
+        foreach (var chr in s)
+        {
+            if (chars.ContainsKey(chr))
+            {
+                chars[chr]++;
+            }
+            else
+            {
+                chars[chr] = 1;
+            }
+        }
+
+        var check = false;
+        foreach (var kvp in chars)
+        {
+            if (kvp.Value % 2 == 0)
+            {
+                polLen += kvp.Value;
+            }
+            else
+            {
+                polLen += kvp.Value - 1;
+                check = true;
+            }
+        }
+
+        return check ? polLen + 1 : polLen;
+    }
+}
