cfaa9b8b9a069c49e9d8877c678b4f7593393666
idoma <h2870132@gmail.com>
2023-03-15 20:25:13 +0200

    BinarySearch

---
+++ Problems.Test/BinarySearchTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Problems.Test;
+
+public class BinarySearchTest
+{
+    private readonly ((int[] Nums, int Target) Input, int Output)[] _testData =
+    {
+        new((new[] { -1, 0, 3, 5, 9, 12 }, 9), 4),
+        new((new[] { -1, 0, 3, 5, 9, 12 }, -1), 0),
+        new((new[] { -1, 0, 3, 5, 9, 12 }, 0), 1),
+        new((new[] { -1, 0, 3, 5, 9, 12 }, 33), -1),
+        new((new[] { -1, 0, 3, 5, 9, 12 }, 12), 5),
+        new((Array.Empty<int>(), 9), -1),
+    };
+
+    [Fact]
+    public void SearchTest()
+    {
+        foreach (var set in _testData)
+        {
+            Assert.Equal(set.Output, BinarySearch.Search(set.Input.Nums, set.Input.Target));
+        }
+    }
+}
---
+++ Problems/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿namespace Problems;
+
+public static class BinarySearch
+{
+    public static int Search(int[] nums, int target)
+    {
+        if (!nums.Any() || target > nums[^1] || target < nums[0])
+        {
+            return -1;
+        }
+
+        var lIndex = 0;
+        var rIndex = nums.Length - 1;
+        while (rIndex >= lIndex)
+        {
+            var checkIndex = lIndex + (rIndex - lIndex) / 2;
+            if (nums[checkIndex] == target)
+            {
+                return checkIndex;
+            }
+
+            if (nums[checkIndex] < target)
+            {
+                lIndex = checkIndex + 1;
+            }
+            else
+            {
+                rIndex = checkIndex - 1;
+            }
+        }
+
+        return -1;
+    }
+}
