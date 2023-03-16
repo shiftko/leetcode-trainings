94239ccd76e24ba2bbf099e8874bc93af4638587
idoma <h2870132@gmail.com>
2023-03-16 20:59:55 +0200

    ValidateBinarySearchTree

---
+++ Problems.Test/ValidateBinarySearchTreeTest.cs
@@ -0,0 +1,27 @@
+﻿using Problems.Shared;
+
+namespace Problems.Test;
+
+public class ValidateBinarySearchTreeTest
+{
+    private readonly Dictionary<int?[], bool> _testData = new()
+    {
+        { new int?[] { 5, 1, 4, null, null, 3, 6 }, false },
+        { new int?[] { 5, 4, 6, null, null, 3, 7 }, false },
+        // { new int?[] { 120, 70, 140, 50, 100, 130, 160, 20, 55, 75, 110, 119, 135, 150, 200 }, false },
+        { new int?[] { 2, 1, 3 }, true },
+        { new int?[] { 2, 2, 2 }, false },
+        { new int?[] { 0, -1 }, true },
+    };
+
+    [Fact]
+    public void GetMaxProfitTest()
+    {
+        foreach (var kvp in _testData)
+        {
+            var node = TreeNodeHelper.CreateBinaryTreeAndReturnHead(kvp.Key);
+            var res = ValidateBinarySearchTree.IsValidBST(node);
+            Assert.Equal(kvp.Value, res);
+        }
+    }
+}
---
+++ Problems/ValidateBinarySearchTree.cs
@@ -0,0 +1,67 @@
+﻿using Problems.Shared;
+
+namespace Problems;
+
+public static class ValidateBinarySearchTree
+{
+    public enum Action
+    {
+        Greater,
+        Less
+    }
+
+    public static bool IsValidBST(TreeNode root)
+    {
+        var nodes = new List<(TreeNode Node, Dictionary<TreeNode, Action> Conditions)>();
+        nodes.Add(new(root, new Dictionary<TreeNode, Action>()));
+
+        var counter = 0;
+        while (counter < nodes.Count)
+        {
+            var current = nodes[counter++];
+            foreach (var condition in current.Conditions)
+            {
+                var action = condition.Value;
+                var prevNode = condition.Key;
+                if (action == Action.Greater)
+                {
+                    if (current.Node.val <= prevNode.val)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (current.Node.val >= prevNode.val)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            var left = current.Node.left;
+            var right = current.Node.right;
+
+            if (left is null && right is null)
+            {
+                continue;
+            }
+
+            if (left is not null)
+            {
+                var leftConditions = new Dictionary<TreeNode, Action>(current.Conditions);
+                leftConditions.Add(current.Node, Action.Less);
+                nodes.Add(new(left, leftConditions));
+            }
+
+            if (right is not null)
+            {
+                var rightConditions = new Dictionary<TreeNode, Action>(current.Conditions);
+                rightConditions.Add(current.Node, Action.Greater);
+                nodes.Add(new(right, rightConditions));
+            }
+        }
+
+        return true;
+    }
+}
