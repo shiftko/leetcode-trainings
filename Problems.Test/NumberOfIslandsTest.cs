9a62c9917f37414ebcf27bbc70ea2c590468c484
idoma <h2870132@gmail.com>
2023-03-17 23:40:21 +0200

    NumberOfIslands

---
+++ Problems.Test/NumberOfIslandsTest.cs
@@ -0,0 +1,35 @@
+﻿namespace Problems.Test;
+
+public class NumberOfIslandsTest
+{
+    [Fact]
+    public void NumIslandsTest()
+    {
+        var payload = new[]
+        {
+            new[] { '1', '1', '1', '1', '0' },
+            new[] { '1', '1', '0', '1', '0' },
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '1', '1', '1', '1', '0' },
+            new[] { '0', '0', '0', '0', '0' },
+        };
+        var res = 1;
+
+        Assert.Equal(res, NumberOfIslands.GetNumIslands(payload));
+    }
+
+    [Fact]
+    public void NumIslandsTest2()
+    {
+        var payload = new[]
+        {
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '1', '1', '0', '0', '0' },
+            new[] { '0', '0', '1', '0', '0' },
+            new[] { '0', '0', '0', '1', '1' },
+        };
+        var res = 3;
+
+        Assert.Equal(res, NumberOfIslands.GetNumIslands(payload));
+    }
+}
---
+++ Problems/NumberOfIslands.cs
@@ -0,0 +1,47 @@
+﻿namespace Problems;
+
+public static class NumberOfIslands
+{
+    public static int GetNumIslands(char[][] grid)
+    {
+        const char land = '1';
+        const char water = '0';
+        var islandСounter = 0;
+        var checkedSet = new List<((int m, int n) cell, HashSet<(int m, int n)> island)>();
+
+        var counter = 0;
+        while (counter < checkedSet.Count)
+        {
+            var cell = checkedSet[counter].cell;
+            var island = checkedSet[counter].island;
+
+            if (grid[cell.m][cell.n] == land)
+            {
+                island.Add(cell);
+            }
+
+            // add next cells to checked set
+            if (cell.m - 1 >= 0)
+            {
+                checkedSet.Add((cell with { m = cell.m - 1 }, island));
+            }
+
+            if (cell.m + 1 < grid.Length)
+            {
+                checkedSet.Add((cell with { m = cell.m + 1 }, island));
+            }
+
+            if (cell.n - 1 >= 0)
+            {
+                checkedSet.Add((cell with { n = cell.n - 1 }, island));
+            }
+
+            if (cell.n + 1 < grid[cell.m].Length)
+            {
+                checkedSet.Add((cell with { n = cell.n + 1 }, island));
+            }
+        }
+
+        return counter;
+    }
+}
