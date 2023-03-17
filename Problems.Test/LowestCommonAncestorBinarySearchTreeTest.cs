567696176153532b7d91181921168d9c0b8bb0c9
idoma <h2870132@gmail.com>
2023-03-17 15:11:14 +0200

    LowestCommonAncestorBinarySearchTree

---
+++ Problems.Test/LowestCommonAncestorBinarySearchTreeTest.cs
@@ -0,0 +1,22 @@
+﻿using Problems.Shared;
+
+namespace Problems.Test;
+
+public class LowestCommonAncestorBinarySearchTreeTest
+{
+    private readonly List<((int?[] root, int p, int q) input, int output)> _testData = new()
+    {
+        (new(new int?[] { 6, 2, 8, 0, 4, 7, 9, null, null, 3, 5 }, 2, 8), 6),
+    };
+
+    [Fact]
+    public void GetMaxProfitTest()
+    {
+        foreach (var set in _testData)
+        {
+            var node = TreeNodeHelper.CreateBinaryTreeAndReturnHead(set.input.root);
+            var res = LowestCommonAncestorBinarySearchTree.LowestCommonAncestor(node, node.left, node.right);
+            Assert.Equal(node, res);
+        }
+    }
+}
---
+++ Problems/LowestCommonAncestorBinarySearchTree.cs
@@ -0,0 +1,63 @@
+﻿using Problems.Shared;
+
+namespace Problems;
+
+public static class LowestCommonAncestorBinarySearchTree
+{
+    public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+    {
+        var nodes = new List<(TreeNode node, HashSet<TreeNode> parents)>();
+        nodes.Add((root, new HashSet<TreeNode>()));
+
+        var foundP = false;
+        var foundQ = false;
+
+        var parentsP = new HashSet<TreeNode>();
+        var parentsQ = new HashSet<TreeNode>();
+
+        var counter = 0;
+        while (counter < nodes.Count && (!foundP || !foundQ))
+        {
+            var node = nodes[counter].node;
+            var parents = nodes[counter++].parents;
+
+            if (node == p)
+            {
+                foundP = true;
+                parentsP = parents;
+            }
+
+            if (node == q)
+            {
+                foundQ = true;
+                parentsQ = parents;
+            }
+
+            if (!foundP || !foundQ)
+            {
+                parents.Add(node);
+                if (node.left is not null)
+                {
+                    nodes.Add((node.left, new HashSet<TreeNode>(parents)));
+                }
+
+                if (node.right is not null)
+                {
+                    nodes.Add((node.right, new HashSet<TreeNode>(parents)));
+                }
+            }
+        }
+
+        if (parentsP.Contains(q))
+        {
+            return q;
+        }
+
+        if (parentsQ.Contains(p))
+        {
+            return p;
+        }
+
+        return (parentsP.Intersect(parentsQ)).LastOrDefault() ?? root;
+    }
+}
