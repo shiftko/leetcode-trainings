91a15970c7adda4be91b3a58472f8e23b646cc1b
idoma <ivan.domaranskiy@hys-enterprise.com>
2023-03-22 16:17:02 +0200

    MinCostClimbingStairs

---
+++ Problems.Test/MinCostClimbingStairsTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Problems.Test;
+
+public class MinCostClimbingStairsTest
+{
+    [Fact]
+    public void GetMinCostClimbingStairsTest()
+    {
+        var payload = new[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+
+        Assert.Equal(6, MinCostClimbingStairs.GetMinCostClimbingStairs(payload));
+    }
+
+    [Fact]
+    public void GetMinCostClimbingStairsTest2()
+    {
+        var payload = new[] { 10, 15, 20 };
+
+        Assert.Equal(15, MinCostClimbingStairs.GetMinCostClimbingStairs(payload));
+    }
+}
---
+++ Problems/MinCostClimbingStairs.cs
@@ -0,0 +1,67 @@
+﻿namespace Problems;
+
+public static class MinCostClimbingStairs
+{
+    public static int GetMinCostClimbingStairs(int[] cost)
+    {
+        var queue = new List<Node>
+        {
+            new Node(0, cost[0], null),
+            new Node(1, cost[1], null)
+        };
+        var lastNodes = new HashSet<Node>();
+
+        var count = 0;
+        while (count < queue.Count)
+        {
+            var index = count++;
+            var currentNode = queue[index];
+
+            var increments = new[] { 1, 2 };
+            foreach (var increment in increments)
+            {
+                var i = currentNode.Index + increment;
+                if (i < cost.Length)
+                {
+                    var nextNode = new Node(i, cost[i], currentNode);
+                    queue.Add(nextNode);
+                }
+                else
+                {
+                    lastNodes.Add(currentNode);
+                }
+            }
+        }
+
+        var costs = new HashSet<int>();
+        foreach (var lastNode in lastNodes)
+        {
+            var currentCost = lastNode.Cost;
+            var parent = lastNode.Parent;
+            while (parent is not null)
+            {
+                currentCost += parent.Cost;
+                parent = parent.Parent;
+            }
+
+            costs.Add(currentCost);
+        }
+
+        return costs.Min();
+    }
+
+    class Node
+    {
+        public readonly int Index;
+        public readonly int Cost;
+
+        public Node? Parent;
+
+        public Node(int index, int cost, Node? parent)
+        {
+            Index = index;
+            Cost = cost;
+            Parent = parent;
+        }
+    }
+}
